11038e8074eb1a4b2d5a659445ad627f1a28d477
Daniel Martel <danielmartelmarrero@gmail.com>
2024-05-31 19:25:18 +0100

    bonus 1 finished

--- index.js
+++ index.js
@@ -43,3 +43,20 @@ else if(compare1===1){
 else{
     console.log("What?! You both have the same name")
 }
+
+//Bonus 1 
+let longText = "Lorem Ipsum is simply dummy text of the printing and et typesetting industry. Lorem et Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+let words = 0;
+let et = 0;
+for(let i=0; i<longText.length; i++){
+    if(longText[i]==" "){
+        words++
+    }
+    if(longText.slice(i,i+4)==" et "){
+        et++
+    }
+}
+console.log(words+1)
+console.log(et)
+//Bonus 2
+let phraseToCheck = 
